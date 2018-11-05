968016d89446ccd0ab61541f87b1a9d762216f13
Anklav <81anklav18@gmail.com>
2018-11-05 18:15:48 +0300

    Changes in ReadDat.class. Now it works correctly.

--- src/sample/ReadDat.java
+++ src/sample/ReadDat.java
@@ -10,11 +10,16 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import sun.jvm.hotspot.jdi.ByteValueImpl;
 
 import java.io.*;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Random;
 import java.util.ResourceBundle;
 
@@ -40,19 +45,22 @@ public class ReadDat extends Line implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {/*...*/}
 
-    XYChart.Series<Float, Float> getSeries() throws IOException {
-        series1 = new XYChart.Series<Float, Float>();
+    private XYChart.Series<Float, Float> getSeries() throws IOException {
+        series1 = new XYChart.Series<>();
 //  read the file into a byte array
-        File file = new File(new File(getCacheDirectory()), "pgp_1ms.dat");
-        FileInputStream fis = new FileInputStream(file);
-        byte [] arr = new byte[(int)file.length()];
-        fis.read(arr);
+        RandomAccessFile file = new RandomAccessFile( getCacheDirectory() + "/pgp_1ms.dat", "r");
+        //FileInputStream fis = new FileInputStream(file);
 
-//  create a byte buffer and wrap the array
-        ByteBuffer bb = ByteBuffer.wrap(arr);
+        float[] fl = new float[1000];
 
-        for (int i = 0; i < (int)file.length()/8; i++) {
-            series1.getData().add(new XYChart.Data<>(Float.valueOf(i), bb.getFloat()));
+        try(FileChannel fc = new RandomAccessFile(getCacheDirectory() + "/pgp_1ms.dat", "rw").getChannel()) {
+            FloatBuffer fb = fc.map(FileChannel.MapMode.READ_WRITE, 0, fc.size())
+                    .order(ByteOrder.nativeOrder()).asFloatBuffer();
+            fb.get(fl);
+        }
+
+        for (int i = 0; i < fl.length; i++) {
+            series1.getData().add(new XYChart.Data<>((float) i, fl[i]));
         }
 
 //        for (int i = 0; i < N; i++)
